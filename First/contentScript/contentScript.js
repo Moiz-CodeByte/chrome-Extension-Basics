d4c7ecc895e34dd06463f3e396236f29575d0f3d
MoizCodeByte <moiz8598@gmail.com>
2024-03-23 19:23:22 +0500

    perission ans storage

--- First/contentScript/contentScript.js
+++ First/contentScript/contentScript.js
@@ -5,3 +5,7 @@ chrome.runtime.sendMessage({Greating : "Hello"},(response) => {
     console.log(response.farewell);
       
   });
+
+chrome.storage.local.set({ name: "Moji" }).then(() => { console.log("Name is set"); });
+
+chrome.storage.local.get(["name"]).then((result) => { console.log("Name is " + result.name); }); 
