f159b440c38b8d3dcb3af90cdb360802d5e35586
MoizCodeByte <moiz8598@gmail.com>
2024-03-25 14:43:33 +0500

    Chrome tabs and tabs events

--- First/background/background.js
+++ First/background/background.js
@@ -24,23 +24,53 @@ console.log("i am from background.js");
 //     }
 //   });
 
-chrome.contextMenus.create({
-    id: 'menu1',
-     title: "Menu 1",
-     contexts: ['all']
-    });
-chrome.contextMenus.create({
-        id: 'menu2',
-         title: "Menu 2",
-         contexts: ['all']
-        });
-chrome.contextMenus.create({
-            id: 'menu3',
-             title: "Menu 3",
-             contexts: ['all']
-            });
-chrome.contextMenus.remove('menu3');
-chrome.contextMenus.onClicked.addListener((info,tab) =>{
-    console.log(info);
+// chrome.contextMenus.create({
+//     id: 'menu1',
+//      title: "Menu 1",
+//      contexts: ['all']
+//     });
+// chrome.contextMenus.create({
+//         id: 'menu2',
+//          title: "Menu 2",
+//          contexts: ['all']
+//         });
+// chrome.contextMenus.create({
+//             id: 'menu3',
+//              title: "Menu 3",
+//              contexts: ['all']
+//             });
+// chrome.contextMenus.remove('menu3');
+// chrome.contextMenus.onClicked.addListener((info,tab) =>{
+//     console.log(info);
+//     console.log(tab);
+    
+// });
+chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+    console.log(tabs);
+    console.log("current tab");
+  });   
+
+chrome.tabs.onCreated.addListener((tab) =>{
+    console.log("tab created");
     console.log(tab);
-})
+});
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) =>{
+    console.log("tab updated");
+    console.log(tabId);
+    console.log(changeInfo);
+    console.log(tab);
+});
+chrome.tabs.onActivated.addListener((activeInfo) =>{
+    console.log("tab activated");
+    console.log(activeInfo);
+});
+chrome.tabs.onRemoved.addListener((tabId, removeInfo) =>{
+    console.log("tab removed");
+    console.log(tabId);
+    console.log(removeInfo);
+});
+chrome.tabs.onMoved.addListener((tabId, moveInfo) =>{
+    console.log("tab moved");
+    console.log(tabId);
+    console.log(moveInfo);
+});
