a9330526fb8b051c0670b2e4ba8b2c504ff8aecd
MoizCodeByte <moiz8598@gmail.com>
2024-04-01 14:24:33 +0500

    Smart Play login fixed button not clicked error

--- Smart play login/contentScript/contentScript.js
+++ Smart play login/contentScript/contentScript.js
@@ -1,18 +1,27 @@
+function Selector(selector, element) {
+  const inputField = document.querySelector(selector);
+  if (inputField) {
+    inputField.focus();
+    document.execCommand('insertText', false, element);
+  } else {
+    console.error(`Input field with selector not found.`);
+  }
+}
+
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  password = request.password;
+  username = request.username;
   if (request.action === 'login') {
-    const usernameInput = document.querySelector('input[name="pc-login-username"]');
-    const passwordInput = document.querySelector('input[name="pc-login-password"]');
-    if (usernameInput && passwordInput) {
-      usernameInput.value = request.username;
-      passwordInput.value = request.password;
-      const loginButton = document.querySelector('[name="pc-login-btn"]');
-      if (loginButton) {
-        loginButton.click();
-      } else {
-        console.error('Login button not found.');
-      }
+    Selector('input[name="pc-login-username"]', username);
+    Selector('input[name="pc-login-password"]', password);
+  
+    const loginButton = document.querySelector('[name="pc-login-btn"]');
+    if (loginButton) {
+      loginButton.click();
     } else {
-      console.error('Login form inputs not found.');
+      console.error('Login button not found.');
     }
+  } else {
+    console.error('Login form inputs not found.');
   }
 });
--- Smart play login/popup/popup.js
+++ Smart play login/popup/popup.js
@@ -1,8 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
+
   document.getElementById('loginBtn').addEventListener('click', function () {
+
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
+    
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+
       var activeTab = tabs[0];
       chrome.tabs.sendMessage(activeTab.id, { action: 'login', username: username, password: password });
     });
